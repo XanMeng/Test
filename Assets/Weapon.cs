60f77ac4d073b4d14406add89b8a0a96e21c99df
XanMeng <xanmengyang@gmail.com>
2020-11-16 20:06:26 +0100

    Add Weapon

---
+++ Assets/Weapon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+
+public class Weapon : MonoBehaviour {
+
+    IEnumerator Start()
+    {
+        for (; ; )
+        {
+            Collider[] colliders = Physics.OverlapBox(transform.position, transform.localScale / 2.0f, transform.rotation);
+
+            colliders.ToList().ForEach(x =>
+            {
+                IDamageable damageable = x.transform.GetComponent<IDamageable>();
+                if (damageable != null)
+                {
+                    Destroy(this.gameObject);
+                }
+
+            });
+
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+}
